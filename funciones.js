4fd86d9bd8c095b036d568ea424dc0ec49ae7615
Angel Dario <angelperez.cmp.b.16@gmail.com>
2020-04-22 22:09:38 -0600

    se establecieron los codigos para hacer que se llevasen a cabo las operacines correspondientes

---
+++ funciones.js
@@ -0,0 +1,44 @@
+function add()
+{
+ var a = document.getElementById("a").value;
+ var b = document.getElementById("b").value;
+ a = parseInt(a);
+ b = parseInt(b);
+ var c;
+ c = parseInt(c);
+ c = a + b;
+document.getElementById("c").value = c;
+}
+function rest()
+{
+ var a = document.getElementById("a").value;
+ var b = document.getElementById("b").value;
+ a = parseInt(a);
+ b = parseInt(b);
+ var c;
+ c = parseInt(c);
+ c = a - b;
+document.getElementById("c").value = c;
+}
+function mul()
+{
+ var a = document.getElementById("a").value;
+ var b = document.getElementById("b").value;
+ a = parseInt(a);
+ b = parseInt(b);
+ var c;
+ c = parseInt(c);
+ c = a * b;
+document.getElementById("c").value = c;
+}
+function div()
+{
+ var a = document.getElementById("a").value;
+ var b = document.getElementById("b").value;
+ a = parseInt(a);
+ b = parseInt(b);
+ var c;
+ c = parseInt(c);
+ c = a / b;
+document.getElementById("c").value = c;
+}
